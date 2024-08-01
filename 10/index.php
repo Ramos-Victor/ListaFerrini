e0c5856c19be99ae66f2ed329683a2b636983b03
Ramos-Victor <vm02oliveiraramos@gmail.com>
2024-08-01 19:30:39 -0300

    Add mais alguns exercicios

---
+++ 10/index.php
@@ -0,0 +1,11 @@
+<legend>Busca Linear</legend>
+<?php
+     $array = [10,5,55,52,34,58,24,25,54,4];
+    
+     echo "Array: ";
+     foreach($array as $number =>$num)
+     {
+         echo "$num ";
+     }
+
+?>
